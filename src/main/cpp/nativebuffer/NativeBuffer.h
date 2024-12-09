a427370fcf561542db4806eae450f0aedc726e98
ccccjiemo <631003561@qq.com>
2024-12-09 13:08:22 +0800

    update 0.2.0-beta

---
+++ src/main/cpp/nativebuffer/NativeBuffer.h
@@ -0,0 +1,14 @@
+//
+// Created on 2024/12/8.
+//
+// Node APIs are not fully supported. To solve the compilation error of the interface cannot be found,
+// please include "napi/native_api.h".
+
+#ifndef GLTEST_NATIVEBUFFER_H
+#define GLTEST_NATIVEBUFFER_H
+
+class NativeBuffer {
+
+};
+
+#endif //GLTEST_NATIVEBUFFER_H
